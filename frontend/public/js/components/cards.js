7d1b716ce57c087aeb69092b3d5e4e7eea93a28d
unknown <karensofia0404021@gmail.com>
2025-07-09 10:26:01 -0500

    Actualizacion de errores

--- frontend/public/js/components/cards.js
+++ frontend/public/js/components/cards.js
@@ -10,3 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch(error => console.error("Error cargando la tarjeta:", error));
   }
 });
+
+//Espera a que cargue todo el DOM, busca el contenedor con clase .card-container, si existe, hace fetch 
+//al archivo cards.html para traer el contenido (el HTML de la tarjeta), inserta la tarjeta adentro 
+//de .card-container.
